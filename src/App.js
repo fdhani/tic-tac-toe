ab9a0d73c51a08229011aec5c974edaf825c2c30
Fachran Dhani Achmad <fachran@99.co>
2024-06-03 19:27:42 +0700

    first changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const Button = ({ children }) => {
+  return <button className="button">{children}</button>;
+};
+
+const Square = ({ value }) => {
+  const [state, setState] = useState(null);
+
+  const handleClick = () => {
+    setState("X");
+    console.log("CLICKED SQUARE NUMBER", value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="square" onClick={handleClick}>
+      {state}
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="board">
+        <div className="row">
+          <Square value={1} />
+          <Square value={2} />
+          <Square value={3} />
+        </div>
+        <div className="row">
+          <Square value={4} />
+          <Square value={5} />
+          <Square value={6} />
+        </div>
+        <div className="row">
+          <Square value={7} />
+          <Square value={8} />
+          <Square value={9} />
+        </div>
+      </div>
+      <div className="game-info">
+        <h2>Next player: X</h2>
+        <Button>Start Over</Button>
+
+        <h2>History</h2>
+        <Button>Langkah #1</Button>
+        <Button>Langkah #2</Button>
+        <Button>Langkah #3</Button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
